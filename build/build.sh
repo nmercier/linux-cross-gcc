5fd3200b0395b1c90b97cf9b21c425c5ff4104e9
yngwe <yngwe@BENDER>
2015-01-05 10:54:33 +0100

    fixed commented out lines

--- build/build.sh
+++ build/build.sh
@@ -42,20 +42,20 @@ popd
 }
 
 
-#build gmp-6.0.0 --disable-shared --enable-static
-#build mpfr-3.1.2 --disable-shared --enable-static --with-gmp=$prefix
-#build mpc-1.0.2 --disable-shared --enable-static --with-gmp=$prefix --with-mpfr=$prefix
-#build binutils-2.24 --target=x86_64-pc-linux-gnu --with-sysroot=$prefix/sysroot/ --with-build-sysroot=$prefix/sysroot/ \
-#					--enable-multiarch --with-multilib-list=m32,m64,mx32 --disable-nls --enable-static --disable-sjlj \
-#					--disable-win32-registry --enable-gold --with-gmp=$prefix --with-mpfr=$prefix --with-mpc=$prefix
-#build gcc-4.9.2 --target=x86_64-pc-linux-gnu --with-sysroot=$prefix/sysroot/ --with-build-sysroot=$prefix/sysroot/ \
-#				--enable-multiarch --with-multilib-list=m32,m64 --disable-nls --enable-static --disable-sjlj \
-#				--disable-win32-registry --enable-gold --with-gmp=$prefix --with-mpfr=$prefix --with-mpc=$prefix \
-#				--enable-objc-gc --enable-languages=c,c++,objc,obj-c++
-#build_clang llvm-3.5.0.src --target=x86_64-pc-linux-gnu --enable-optimized=yes --enable-assertions=no \
-#						   --enable-keep-symbols=no --enable-docs=no --program-prefix= --enable-targets=all \
-#						   --with-sysroot=$prefix/sysroot --with-default-sysroot=$prefix/sysroot \
-#						   --with-gcc-toolchain=$prefix
+build gmp-6.0.0 --disable-shared --enable-static
+build mpfr-3.1.2 --disable-shared --enable-static --with-gmp=$prefix
+build mpc-1.0.2 --disable-shared --enable-static --with-gmp=$prefix --with-mpfr=$prefix
+build binutils-2.24 --target=x86_64-pc-linux-gnu --with-sysroot=$prefix/sysroot/ --with-build-sysroot=$prefix/sysroot/ \
+					--enable-multiarch --with-multilib-list=m32,m64,mx32 --disable-nls --enable-static --disable-sjlj \
+					--disable-win32-registry --enable-gold --with-gmp=$prefix --with-mpfr=$prefix --with-mpc=$prefix
+build gcc-4.9.2 --target=x86_64-pc-linux-gnu --with-sysroot=$prefix/sysroot/ --with-build-sysroot=$prefix/sysroot/ \
+				--enable-multiarch --with-multilib-list=m32,m64 --disable-nls --enable-static --disable-sjlj \
+				--disable-win32-registry --enable-gold --with-gmp=$prefix --with-mpfr=$prefix --with-mpc=$prefix \
+				--enable-objc-gc --enable-languages=c,c++,objc,obj-c++
+build_clang llvm-3.5.0.src --target=x86_64-pc-linux-gnu --enable-optimized=yes --enable-assertions=no \
+						   --enable-keep-symbols=no --enable-docs=no --program-prefix= --enable-targets=all \
+						   --with-sysroot=$prefix/sysroot --with-default-sysroot=$prefix/sysroot \
+						   --with-gcc-toolchain=$prefix
 build_gdb gdb-7.8.1 --target=x86_64-pc-linux-gnu --with-gmp=$prefix --with-mpfr=$prefix --with-mpc=$prefix \
 					--enable-multiarch --with-multilib-list=m32,m64 --disable-nls --enable-languages=c,c++,objc,obj-c++ \
 					--enable-shared --enable-static --disable-sjlj --disable-win32-registry
